f911d021aa0810558bb3bf8fa33ae99229f17498
yi0805ian <ytan960@aucklanduni.ac.nz>
2026-02-12 15:24:07 +1300

    Refactor UI components and enhance styling with Tailwind CSS

---
+++ src/features/user/UserName.jsx
@@ -0,0 +1,5 @@
+function UserName() {
+  return <span className="hidden text-sm font-semibold md:block">Tang</span>;
+}
+
+export default UserName;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,15 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-import SearchOrder from "../features/order/SearchOrder";
+import SearchOrder from '../features/order/SearchOrder';
+import UserName from '../features/user/UserName';
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="font-pizza flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
 
       <SearchOrder />
 
-      <p>Tang</p>
+      {<UserName />}
     </header>
   );
 }
