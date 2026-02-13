09f168060cb444d8bb0e1c37a4dfdd13d70c30fc
yi0805ian <ytan960@aucklanduni.ac.nz>
2026-02-13 18:28:09 +1300

    Add UpdateOrder component to enable priority updates for orders

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -9,6 +9,7 @@ import {
   formatDate,
 } from '../../utils/helpers';
 import { useEffect } from 'react';
+import UpdateOrder from './UpdateOrder';
 
 function Order() {
   const order = useLoaderData();
@@ -88,6 +89,8 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
